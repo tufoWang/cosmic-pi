4f22862908c229ce011255e5327699660cd8a390
KOSASIH <kosasihg88@gmail.com>
2025-01-04 11:45:36 +0700

    Create Footer.js

---
+++ src/frontend/components/Footer.js
@@ -0,0 +1,22 @@
+// components/Footer.js
+
+<template>
+    <footer>
+        <p>&copy; 2023 Cosmic Pi. All rights reserved.</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+};
+</script>
+
+<style scoped>
+footer {
+    text-align: center;
+    padding: 10px 0;
+    background: #007bff;
+    color: white;
+}
+</style>
