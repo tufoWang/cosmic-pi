10766588ff865d93a72f1a20d45cdb7f00a5982d
KOSASIH <kosasihg88@gmail.com>
2025-01-04 10:55:36 +0700

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+# src/constants.py
+
+# Pi Coin Configuration
+PI_COIN_SYMBOL = "Pi"  # Symbol for Pi Coin
+PI_COIN_VALUE = 314159  # Fixed value of Pi Coin in USD
+PI_COIN_SUPPLY = 100_000_000_000  # Total supply of Pi Coin
+
+# Other constants can be added here as needed
